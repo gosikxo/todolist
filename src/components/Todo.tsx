a6cd7258fe3a94d659b68b67be7422a2caee41e6
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-09-16 13:59:29 +0300

    Add delete button to each task

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -5,6 +5,7 @@ export const Todo = (props: {onCheck: () => void, name: string, id: number, isDo
     <div>
       <input onChange={props.onCheck} checked={props.isDone} type="checkbox"></input>
       <span className={props.isDone ? 'clicked': "notClicked"}>{props.name}</span>
+      <img className='taskDelete' src="https://img.icons8.com/officexs/16/000000/delete-sign.png"/>
     </div> 
 )
 }
